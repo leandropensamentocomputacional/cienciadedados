650814b89853b2a8f8f864d78d18e438849f4c45
Leandro Valk <106777426+leandropensamentocomputacional@users.noreply.github.com>
2024-10-31 18:01:42 +0000

    1

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -10,8 +10,8 @@ async function vizualizarInformacoesGlobais() {
     const paragrafo= document.createElement('p')
     paragrafo.classList.list.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> 
-    de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas
-     em alguma rede social e passam em média <span>${dados.tempo_medio} </span> horas conectadas.`
+    de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas 
+    em alguma rede social e passam em média <span>${dados.tempo_medio} </span> horas conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 
